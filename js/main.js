9778f11c210260e65b8b12b1a3291c0b17794828
barnez29 <barnaby.davids@gmail.com>
2020-03-09 14:17:10 +0200

    javascript tutorial

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// weather app using if statements//
+
+// let weather = prompt("what is the weather condition outside?");
+// console.log(weather)
+
+// if (weather === "sunny") {
+//     console.log("It is " + weather + " wear slippers and shorts");
+
+// } else if (weather === "rainy") {
+//     console.log("It is " + weather + " stay indoors or take umbrella");
+
+// } else if (weather === "windy") {
+//     console.log("It is " + weather + " put on a nice winter jacket its windy outside");
+
+// } else if (weather === "snowy") {
+//     console.log("It is " + weather + " dress warm - its snowing outside");
+
+// } else {
+//     console.log("invalid weather options chosen")
+
+
+// weather app using switch statement//
+
+
+let weather = prompt("what is the weather outside");
+
+switch (weather) {
+    case 'sunny':
+        console.log("wear shorts");
+        break;
+
+    case 'windy':
+        console.log("its windy outside");
+        break;
+
+    case 'rainy':
+        console.log("raining take umbrella");
+        break;
+
+    case 'snowy':
+        console.log("wear jacket snowing outside");
+        break;
+
+    default:
+        console.log("invalid weather options");
+}
